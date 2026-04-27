b634e613f2438c97743721af7c4ef2d04f095574
Aarif <adebayoabdulazeez195@gmail.com>
2026-04-27 22:16:00 +0100

    add global validation pipe and DTO decorators

--- src/card/dto/validate-card.dto.ts
+++ src/card/dto/validate-card.dto.ts
@@ -1,9 +1,8 @@
-// import { IsString, IsNotEmpty } from 'class-validator';
-
-import { IsString, IsNotEmpty } from '@nestjs/class-validator';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class ValidateCardDto {
-  @IsString()
   @IsNotEmpty()
-  cardNumber!: string;
+  @IsString()
+  @MinLength(13)
+  cardNumber: string;
 }

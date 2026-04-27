866a0c7ecb06b56eb5a0ce66c88e0d290355a988
Aarif <adebayoabdulazeez195@gmail.com>
2026-04-27 21:40:32 +0100

    create card validation endpoint and DTO

--- src/card/card.controller.ts
+++ src/card/card.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CardService } from './card.service';
+import { ValidateCardDto } from './dto/validate-card.dto';
 
-@Controller('card')
-export class CardController {}
+@Controller('cards')
+export class CardController {
+  constructor(private readonly cardService: CardService) {}
+
+  @Post('validate')
+  validateCard(@Body() validateCardDto: ValidateCardDto) {
+    return this.cardService.validateCardNumber(validateCardDto.cardNumber);
+  }
+}

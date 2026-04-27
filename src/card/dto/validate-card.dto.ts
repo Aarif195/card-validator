3ba614bee6065b4c613bf5633813bb39b659894b
Aarif <adebayoabdulazeez195@gmail.com>
2026-04-27 21:55:18 +0100

    installed nestjs class-validator

--- src/card/dto/validate-card.dto.ts
+++ src/card/dto/validate-card.dto.ts
@@ -1,7 +1,9 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+// import { IsString, IsNotEmpty } from 'class-validator';
+
+import { IsString, IsNotEmpty } from '@nestjs/class-validator';
 
 export class ValidateCardDto {
   @IsString()
   @IsNotEmpty()
-  cardNumber: string;
+  cardNumber!: string;
 }
